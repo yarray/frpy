d33aa4946c37b72bfe13719aa1be52d6d566de22
yarray <08to09@gmail.com>
2019-10-15 21:00:07 +0800

    expose once in api

--- frpy/api.py
+++ frpy/api.py
@@ -2,7 +2,7 @@
 
 from .core import Stream, clock
 from .unary import fmap, scan, changed, \
-    where, trace, flatten, diff, each, fmap_async, skip
+    where, trace, flatten, diff, each, fmap_async, skip, once
 from .producer import sequence, repeat
 from .timely import timeout, delay
 from .multiary import merge
